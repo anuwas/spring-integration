8fa5ee52c98f5f3f2ae9b20c3c9c2daa114e73ed
ANUWAS <info.anuwas@gmail.com>
2018-12-17 01:21:39 +0530

    Commit Header Enricher

--- src/main/java/com/anu/springintegration/SpringIntegrationApplication.java
+++ src/main/java/com/anu/springintegration/SpringIntegrationApplication.java
@@ -1,5 +1,7 @@
 package com.anu.springintegration;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 import org.springframework.boot.SpringApplication;
@@ -7,7 +9,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.annotation.Transformer;
 import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.transformer.HeaderEnricher;
+import org.springframework.integration.transformer.support.HeaderValueMessageProcessor;
+import org.springframework.integration.transformer.support.StaticHeaderValueMessageProcessor;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
@@ -28,15 +34,34 @@ public class SpringIntegrationApplication {
 		}
 		scanner.close();
 	}
-	
+	//this is input channel
 	@Bean
 	public MessageChannel messagechannel() {
 		return new DirectChannel();
 	}
 	
+	//this is output channel
+	@Bean
+	public MessageChannel messageoutchannel() {
+		return new DirectChannel();
+	}
+		
+	// Header enricher using to modify the header of the message
+	@Transformer(inputChannel="messagechannel",outputChannel="messageoutchannel")
+	@Bean
+	public HeaderEnricher headerenricher() {
+		Map<String,HeaderValueMessageProcessor<?>> mp = new HashMap<>();
+		mp.put("key1",new StaticHeaderValueMessageProcessor<String>("firstHeader"));
+		mp.put("key2",new StaticHeaderValueMessageProcessor<String>("SecondHeader"));
+		
+		
+		HeaderEnricher headerEnricher = new HeaderEnricher(mp);
+		return headerEnricher;
+	}
+	
 	//any customization needs to do inside the service activator
 	@Bean
-	@ServiceActivator(inputChannel="messagechannel")
+	@ServiceActivator(inputChannel="messageoutchannel")
 	public MessageHandler messageHandler() {
 		return  (Message<?> message)->System.out.print(message);
 	}
