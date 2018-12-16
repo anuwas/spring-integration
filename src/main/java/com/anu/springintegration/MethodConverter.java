7dfb42d7a8f4c832f7249ed921710990f8e3cd13
ANUWAS <info.anuwas@gmail.com>
2018-12-17 00:06:53 +0530

    Transformer First Commit

---
+++ src/main/java/com/anu/springintegration/MethodConverter.java
@@ -0,0 +1,7 @@
+package com.anu.springintegration;
+
+public class MethodConverter {
+	public String convert(String msg) {
+		return "Modified "+msg+" modified";
+	}
+}
--- src/main/java/com/anu/springintegration/SpringIntegrationApplication.java
+++ src/main/java/com/anu/springintegration/SpringIntegrationApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.annotation.Transformer;
 import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.transformer.MessageTransformingHandler;
+import org.springframework.integration.transformer.MethodInvokingTransformer;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
@@ -34,9 +37,33 @@ public class SpringIntegrationApplication {
 		return new DirectChannel();
 	}
 	
+	// creating new output channel
+	@Bean
+	public MessageChannel messageOutputchannel() {
+		return new DirectChannel();
+	}
+	
+	// this is using to transform the message
+	@Transformer(inputChannel="messagechannel")
+	@Bean
+	public MessageHandler transformer() {
+		MethodInvokingTransformer invokingTransformer = new MethodInvokingTransformer(methodconverter(), "convert");
+		MessageTransformingHandler  transformHandler = new MessageTransformingHandler(invokingTransformer);
+		//setting message to an output channel
+		//No output channel is here , so creating a new one
+		transformHandler.setOutputChannel(messageOutputchannel());
+		return transformHandler;
+	}
+	
+	// creating bean for the converter class
+	@Bean
+	public MethodConverter methodconverter() {
+		return new MethodConverter();
+	}
+	
 	//any customization needs to do inside the service activator
 	@Bean
-	@ServiceActivator(inputChannel="messagechannel")
+	@ServiceActivator(inputChannel="messageOutputchannel")
 	public MessageHandler messageHandler() {
 		return  (Message<?> message)->System.out.print(message);
 	}
