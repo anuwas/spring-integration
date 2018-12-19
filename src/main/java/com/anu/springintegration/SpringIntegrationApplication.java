d018ff1a9c1ae9b51cf7fcbcb282634dc994107d
ANUWAS <info.anuwas@gmail.com>
2018-12-20 01:36:43 +0530

    Recipient List Router commit

--- src/main/java/com/anu/springintegration/SpringIntegrationApplication.java
+++ src/main/java/com/anu/springintegration/SpringIntegrationApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.Router;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageSelector;
+import org.springframework.integration.router.RecipientListRouter;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
@@ -34,12 +37,51 @@ public class SpringIntegrationApplication {
 		return new DirectChannel();
 	}
 	
-	//any customization needs to do inside the service activator
+	@Router(inputChannel="messagechannel")
 	@Bean
-	@ServiceActivator(inputChannel="messagechannel")
-	public MessageHandler messageHandler() {
-		return  (Message<?> message)->System.out.print(message);
+	public RecipientListRouter recipientListRouter() {
+		RecipientListRouter rlr = new RecipientListRouter();
+		/*
+		rlr.addRecipient("savingChannel", new MessageSelector() {
+
+			@Override
+			public boolean accept(Message<?> message) {
+				return false;
+			}
+		});
+		*/
+		//using lambda
+		rlr.addRecipient("savingChannel", (message)->message.getPayload().toString().equalsIgnoreCase("saving"));
+		rlr.setDefaultOutputChannel(deafultOutChannel());
+		return rlr;
+		
 	}
+	
+	@Bean
+	public MessageChannel savingChannel() {
+		return new DirectChannel();
+	}
+	
+	@Bean
+	public MessageChannel deafultOutChannel() {
+		return new DirectChannel();
+	}
+	
+	//any customization needs to do inside the service activator
+	
+
+	@Bean
+	@ServiceActivator(inputChannel="savingChannel")
+	public MessageHandler savingChannelHandler() {
+		return  (Message<?> message)->System.out.print("Saving Channel "+message);
+	}
+	
+	@Bean
+	@ServiceActivator(inputChannel="deafultOutChannel")
+	public MessageHandler deafultOutChannelHandler() {
+		return  (Message<?> message)->System.out.print("default channel"+message);
+	}
+	
 
 }
 
